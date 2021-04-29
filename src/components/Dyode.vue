d71a321166904dd70850b0cd52e0df3125392658
rameyrobo <rameyrobo@gmail.com>
2021-04-28 21:24:58 -0700

    Added initial carousel

--- src/components/Dyode.vue
+++ src/components/Dyode.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="hello">
+  <swiper ref="mySwiper" :options="swiperOptions">
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide>
+    <swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide>
+    <swiper-slide>Slide 5</swiper-slide>
+    <div class="swiper-pagination" slot="pagination"></div>
+  </swiper>
      <row :gutter="12">
         <column :xs="12" :md="4" :lg="3">xs=12 md=4 lg=3</column>
         <column :xs="12" :md="4" :lg="3">xs=12 md=4 lg=3</column>
