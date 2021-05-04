b681cf3c4dc7eb1da010b569df07c8780aaa515f
rameyrobo <rameyrobo@gmail.com>
2021-05-04 04:31:11 -0700

    Changed FullCTA to FullHero for semantic recognition

---
+++ src/components/FullHero.vue
@@ -0,0 +1,34 @@
+<template>
+<div class="full-hero">
+<section class="block-section-desktop">
+    <row :gutter="12">
+     <column :xs="12">
+     <h2 class="header-title section-title">Our Favorite Tees</h2>
+     <h4 class="secondary-text">Everyday tees you need!</h4>
+     <button>Shop Now</button>
+     </column>
+    </row>
+</section>
+</div>
+</template>
+<style lang="scss" scoped>
+@import '../assets/styles/element-variables.scss';
+@media screen and (max-width: 767px) {
+.block-section-desktop {
+    display: none;
+  }
+}
+.full-hero {
+  background-image: url('../assets/images/compressed/CTA-Banner.jpeg');
+  min-height:227px;
+  background-position: center;
+  background-size: cover;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  color: white;
+  box-sizing: border-box;
+  padding: 0 80px;
+  background-color: transparent;
+}
+</style>
