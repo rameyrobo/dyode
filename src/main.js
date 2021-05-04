2854ba2d9d823347503e6aca7fb2abf9acc6ac09
rameyrobo <rameyrobo@gmail.com>
2021-05-04 04:26:59 -0700

    Added element UI scss to main project js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
+import Element from 'element-ui'
+import './assets/styles/element-variables.scss'
 import App from './App.vue'
 
+Vue.use(Element)
 Vue.config.productionTip = false
 
 new Vue({
