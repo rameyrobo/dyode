ae1a6e2fa6dbc9e6031dff934545ed8f4d7cce7e
rameyrobo <rameyrobo@gmail.com>
2021-05-04 04:29:48 -0700

    Updated styles on category section

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,20 +1,32 @@
    <template>
-   <div class="secondary">
+   <div class="secondary container">
     <section class="categories">
     <row :gutter="12">
-        <column :xs="6" :md="4">
+        <column :xs="6" :lg="4">
+        <div class="card-top">
           <div class="card card-1">
+          </div>
+          <div class="card-title">
           Women's
           </div>
-        </column>
-        <column :xs="6" :md="4">
-          <div class="card card-2">
-          Men's
           </div>
         </column>
-        <column :xs="12" :md="4">
+        <column :xs="6" :lg="4">
+        <div class="card-top">
+          <div class="card card-2">
+          </div>
+          <div class="card-title">
+          Men's
+          </div>
+          </div>
+        </column>
+        <column :xs="12" :lg="4">
+        <div class="card-bottom">
           <div class="card card-3">
+          </div>
+          <div class="card-title">
           Accessories
+          </div>
           </div>
         </column>
     </row>
@@ -22,30 +34,48 @@
     </div>
 </template>
 <style lang="scss">
+@import '../assets/styles/element-variables.scss';
+.secondary {
+  margin: 2rem auto 10px;
+}
+
 // Card Styles
 
 .card {
   width: 100%;
-  background-position: center;
+  background-position: top;
   background-size: cover;
   display: flex;
   flex-direction: column;
   justify-content: center;
   color: white;
   box-sizing: border-box;
-  padding: 0 80px;
   background-color: transparent;
-  min-height:600px;
   height:100%;
-
+  min-height: 203px;
   &.card-1 {
-    background-image:url('../assets/images/compressed/Accessories-Image-1.jpeg');
+    background-image:url('../assets/images/compressed/Accessories-Image-1.jpeg')
   }
   &.card-2 {
-   background-image:url('../assets/images/compressed/Accessories-Image-2.jpeg');
+    background-image:url('../assets/images/compressed/Accessories-Image-2.jpeg')
   }
   &.card-3 {
-    background-image:url('../assets/images/compressed/Accessories-Image-3.jpeg');
+    background-image:url('../assets/images/compressed/Accessories-Image-3.jpeg')
+  }
+}
+  .card-title {
+    background:$main-color;
+    font-family: 'Pacifico', cursive;
+    color: $color-white;
+    font-size: 2.5rem;
+    min-height:42px;
+  }
+@media (min-width: $layout-breakpoint-medium) {
+.card {
+  min-height:400px;
+  &.card-3{
+    background-image:url('../assets/images/compressed/Accessories-Image-3-Full.jpeg');
+    }
   }
 }
 </style>
