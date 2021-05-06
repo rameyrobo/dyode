d429f425507a313b5a6e5402247ccbfbf1ddd2df
rameyrobo <rameyrobo@gmail.com>
2021-05-06 14:35:48 -0700

    Adjusted gutter widths to proper size

--- src/components/Instagram.vue
+++ src/components/Instagram.vue
@@ -1,28 +1,42 @@
 <template>
-<div class="instagram">
-<h2 class="header-title section-title">Follow Us On Instagram</h2>
+<div class="instagram secondary container">
+<h2 class="header-title section-title ig">Follow Us On Instagram</h2>
 <section>
 <row :gutter="0" :columns="30">
         <column :xs="10" :sm="10" :md="10" :lg="6"><img src="../assets/images/compressed/Social-1.jpeg" class="social-img" /></column>
         <column :xs="10" :sm="10" :md="10" :lg="6"><img src="../assets/images/compressed/Social-2.jpeg" class="social-img" /></column>
         <column :xs="10" :sm="10" :md="10" :lg="6"><img src="../assets/images/compressed/Social-3.jpeg" class="social-img" /></column>
-        <column class="hiddenVGR-lg" :xs="0" :sm="0" :md="6" :lg="6"><img src="../assets/images/compressed/Social-4.jpeg" class="social-img" /></column>
-        <column class="hiddenVGR-lg" :xs="0" :sm="0" :md="6" :lg="6"><img src="../assets/images/compressed/Social-5.jpeg" class="social-img" /></column>
+        <column class="hiddenVGR-lg-mf" :xs="0" :sm="0" :md="6" :lg="6"><img src="../assets/images/compressed/Social-4.jpeg" class="social-img" /></column>
+        <column class="hiddenVGR-lg-mf" :xs="0" :sm="0" :md="6" :lg="6"><img src="../assets/images/compressed/Social-5.jpeg" class="social-img" /></column>
     </row>
 </section>
 </div>
 </template>
 <style lang="scss" scoped>
 @import '../assets/styles/element-variables.scss';
+.header-title.section-title.ig {
+    @include font-size(2.235rem);
+}
 .instagram, .instagram .container {
     overflow: hidden;
 }
 .social-img {
-    max-width: 25vw;
+    max-width: 28.3vw;
 }
-@media screen and (min-width: 992px) {
-.social-img {
-    max-width: 15vw;
+.hiddenVGR-lg-mf {
+    display: none;
 }
+@media screen and (min-width: $layout-breakpoint-medium) { 
+    .header-title.section-title.ig{
+        @include font-size(3.75rem);
+    }
+}
+@media screen and (min-width: $layout-breakpoint-large) {
+    .social-img {
+        max-width: 17.1vw;
+    }
+    .hiddenVGR-lg-mf {
+        display: inline;
+    }
 }
 </style>
