136f6124759b67ccd5c9c3f6915e9933d624a625
rameyrobo <rameyrobo@gmail.com>
2021-05-06 00:18:25 -0700

    Adjusted column sizes to work in mobile to desktop

--- src/components/Instagram.vue
+++ src/components/Instagram.vue
@@ -2,19 +2,27 @@
 <div class="instagram">
 <h2 class="header-title section-title">Follow Us On Instagram</h2>
 <section>
-<row :gutter="12" :columns="10">
-        <column :xs="3" :sm="3" :md="3" :lg="2"><img src="../assets/images/compressed/Social-1.jpeg" class="social-img" /></column>
-        <column :xs="3" :sm="3" :md="3" :lg="2"><img src="../assets/images/compressed/Social-2.jpeg" class="social-img" /></column>
-        <column :xs="3" :sm="3" :md="3" :lg="2"><img src="../assets/images/compressed/Social-3.jpeg" class="social-img" /></column>
-        <column class="hiddenVGR-lg" :xs="0" :sm="0" :md="3" :lg="2"><img src="../assets/images/compressed/Social-4.jpeg" class="social-img" /></column>
-        <column class="hiddenVGR-lg" :xs="0" :sm="0" :md="3" :lg="2"><img src="../assets/images/compressed/Social-5.jpeg" class="social-img" /></column>
+<row :gutter="0" :columns="30">
+        <column :xs="10" :sm="10" :md="10" :lg="6"><img src="../assets/images/compressed/Social-1.jpeg" class="social-img" /></column>
+        <column :xs="10" :sm="10" :md="10" :lg="6"><img src="../assets/images/compressed/Social-2.jpeg" class="social-img" /></column>
+        <column :xs="10" :sm="10" :md="10" :lg="6"><img src="../assets/images/compressed/Social-3.jpeg" class="social-img" /></column>
+        <column class="hiddenVGR-lg" :xs="0" :sm="0" :md="6" :lg="6"><img src="../assets/images/compressed/Social-4.jpeg" class="social-img" /></column>
+        <column class="hiddenVGR-lg" :xs="0" :sm="0" :md="6" :lg="6"><img src="../assets/images/compressed/Social-5.jpeg" class="social-img" /></column>
     </row>
 </section>
 </div>
 </template>
 <style lang="scss" scoped>
 @import '../assets/styles/element-variables.scss';
-header {
-
+.instagram, .instagram .container {
+    overflow: hidden;
+}
+.social-img {
+    max-width: 25vw;
+}
+@media screen and (min-width: 992px) {
+.social-img {
+    max-width: 15vw;
+}
 }
 </style>
