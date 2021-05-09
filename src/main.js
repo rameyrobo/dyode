050ece9b47dfd84331dc472c15d198297b28560f
rameyrobo <rameyrobo@gmail.com>
2021-05-08 18:58:34 -0700

    Removed NPM Burger Menu, installed Vuetify

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
-import './assets/styles/element-variables.scss'
 import 'vue-simple-accordion/dist/vue-simple-accordion.css';
+import vuetify from '@/plugins/vuetify' 
+import './assets/styles/element-variables.scss'
 import App from './App.vue'
 
 Vue.config.productionTip = false
@@ -8,4 +9,5 @@ Vue.config.productionTip = false
 
 new Vue({
   render: function (h) { return h(App) },
+  vuetify,
 }).$mount('#app')
